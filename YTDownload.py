1c19df03ee76716ed86e30d62cb8e987048644a2
Donald Thai <donaldfromthailand73@gmail.com>
2021-11-28 23:15:28 -0600

    Fixed YouTube's download throttling

--- YTDownload.py
+++ YTDownload.py
@@ -1,25 +1,27 @@
 from __future__ import unicode_literals
-import youtube_dl
+import yt_dlp
 from tkinter import Tk, Canvas, Button, Frame, Label, PhotoImage, Toplevel, Entry, Text, END, Scrollbar, filedialog, \
    messagebox
 from PIL import Image, ImageTk, ImageSequence
 import glob
+import ctypes
 
 
 class Gif:
    def __init__(self, parent, gif_loc):
       self.parent = parent
-      self.canvas = Canvas(parent, width=480, height=270, bg="white", highlightthickness=0)
-      self.canvas.place(relwidth=1)
+      self.canvas = Canvas(parent, width = 480, height = 270, bg = "white",
+                           highlightthickness = 0)
+      self.canvas.place(relwidth = 1)
       self.sequence = [ImageTk.PhotoImage(img)
                        for img in ImageSequence.Iterator(
             Image.open(
                gif_loc))]
-      self.image = self.canvas.create_image(0, 0, image=self.sequence[0], anchor="nw")
+      self.image = self.canvas.create_image(0, 0, image = self.sequence[0], anchor = "nw")
       self.animate(1)
 
    def animate(self, counter):
-      self.canvas.itemconfig(self.image, image=self.sequence[counter])
+      self.canvas.itemconfig(self.image, image = self.sequence[counter])
       self.parent.after(35, lambda: self.animate((counter + 1) % len(self.sequence)))
 
 
@@ -28,28 +30,30 @@ class Welcome:
       self.parent = parent
       self.parent.title("YT Downloader by Donald Thai")
       self.parent.geometry("600x600")
-      self.parent.resizable(width=False, height=False)
+      self.parent.resizable(width = False, height = False)
 
-      self.picture = PhotoImage(file="Pictures/Cherry Blossom Railroad.png")
-      self.outside_pic = Label(self.parent, image=self.picture)
-      self.outside_pic.place(relwidth=1, relheight=1)
+      self.picture = PhotoImage(file = "Pictures/Cherry Blossom Railroad.png")
+      self.outside_pic = Label(self.parent, image = self.picture)
+      self.outside_pic.place(relwidth = 1, relheight = 1)
 
-      self.center_frame = Frame(parent, bg="white", bd=0)
-      self.center_frame.place(relx=.1, rely=.1, relwidth=.8, relheight=.8)
-      self.inside_pic = Label(self.center_frame, image=self.picture)
-      self.inside_pic.place(relwidth=1, relheight=1)
+      self.center_frame = Frame(parent, bg = "white", bd = 0)
+      self.center_frame.place(relx = .1, rely = .1, relwidth = .8, relheight = .8)
+      self.inside_pic = Label(self.center_frame, image = self.picture)
+      self.inside_pic.place(relwidth = 1, relheight = 1)
 
-      self.gif_frame = Frame(self.center_frame, bd=0)
-      self.gif_frame.place(relwidth=1, relheight=.55)
+      self.gif_frame = Frame(self.center_frame, bd = 0)
+      self.gif_frame.place(relwidth = 1, relheight = .55)
       self.gif = Gif(self.gif_frame, "Gifs/cat-loading.gif")  # Maybe add a gif cycler?
       self.gif_num = 0
-      self.gif_cycle_button = Button(self.parent, text="Next", command=self.NextGif)
-      self.gif_cycle_button.place(rely=.1, relwidth=.1, relheight=.05)
+      self.gif_cycle_button = Button(self.parent, text = "Next", command = self.NextGif)
+      self.gif_cycle_button.place(rely = .1, relwidth = .1, relheight = .05)
 
-      self.video_button = Button(self.center_frame, text="Download a video", command=self.OpenVidDown)
-      self.video_button.place(relx=.3, rely=.6, relwidth=.4, relheight=.1)
-      self.playlist_button = Button(self.center_frame, text="Download a playlist", command=self.OpenPLDown)
-      self.playlist_button.place(relx=.3, rely=.75, relwidth=.4, relheight=.1)
+      self.video_button = Button(self.center_frame, text = "Download a video",
+                                 command = self.OpenVidDown)
+      self.video_button.place(relx = .3, rely = .6, relwidth = .4, relheight = .1)
+      self.playlist_button = Button(self.center_frame, text = "Download a playlist",
+                                    command = self.OpenPLDown)
+      self.playlist_button.place(relx = .3, rely = .75, relwidth = .4, relheight = .1)
 
    def OpenVidDown(self):
       self.parent.withdraw()
@@ -78,43 +82,46 @@ class DownVid(Toplevel):
       self.directory = ""
       self.geometry("551x600")
       self.title("Video Downloader")
-      self.resizable(width=False, height=False)
+      # self.resizable(width=False, height=False)
 
-      self.picture = PhotoImage(file="Pictures/space dog.png")
-      self.outside_pic = Label(self, image=self.picture)
-      self.outside_pic.place(relwidth=1, relheight=1)
+      self.picture = PhotoImage(file = "Pictures/space dog.png")
+      self.outside_pic = Label(self, image = self.picture)
+      self.outside_pic.place(relwidth = 1, relheight = 1)
 
-      self.center_frame = Frame(self, bd=0)
-      self.center_frame.place(relx=.1, rely=.1, relwidth=.8, relheight=.8)
+      self.center_frame = Frame(self, bd = 0)
+      self.center_frame.place(relx = .1, rely = .1, relwidth = .8, relheight = .8)
 
-      self.inside_pic = Label(self.center_frame, image=self.picture)
-      self.inside_pic.place(relwidth=1, relheight=1)
+      self.inside_pic = Label(self.center_frame, image = self.picture)
+      self.inside_pic.place(relwidth = 1, relheight = 1)
 
-      self.enter_url = Entry(self.center_frame, bd=0)
-      self.enter_url.place(relwidth=.85, relheight=.05)
-      self.get_url = Button(self.center_frame, text="Get Url", command=self.getUrl)
-      self.get_url.place(relx=.87, relwidth=.13, relheight=.05)
+      self.enter_url = Entry(self.center_frame, bd = 0)
+      self.enter_url.place(relwidth = .85, relheight = .05)
+      self.get_url = Button(self.center_frame, text = "Get Url", command = self.getUrl)
+      self.get_url.place(relx = .87, relwidth = .13, relheight = .05)
 
-      self.vid_info = Text(self.center_frame, wrap="word")
-      self.vid_info.place(rely=.1, relwidth=.975, relheight=.65)
-      scrollbar = Scrollbar(self.center_frame, command=self.vid_info.yview)
-      scrollbar.place(relx=.975, rely=.1, relwidth=.025, relheight=.65)
+      self.vid_info = Text(self.center_frame, wrap = "word")
+      self.vid_info.place(rely = .1, relwidth = .975, relheight = .65)
+      scrollbar = Scrollbar(self.center_frame, command = self.vid_info.yview)
+      scrollbar.place(relx = .975, rely = .1, relwidth = .025, relheight = .65)
       self.vid_info["yscrollcommand"] = scrollbar.set
 
-      self.dir_button = Button(self.center_frame, text="Download to", command=self.getDirectory)
-      self.dir_button.place(rely=.8, relwidth=.25, relheight=.05)
+      self.dir_button = Button(self.center_frame, text = "Download to",
+                               command = self.getDirectory)
+      self.dir_button.place(rely = .8, relwidth = .25, relheight = .05)
       self.display_dir = Text(self.center_frame)
-      self.display_dir.place(relx=.3, rely=.8, relwidth=.7, relheight=.05)
+      self.display_dir.place(relx = .3, rely = .8, relwidth = .7, relheight = .05)
 
-      self.download_mp4_button = Button(self.center_frame, text="Download as MP4", command=self.downloadMP4)
-      self.download_mp4_button.place(rely=.875, relwidth=.25, relheight=.05)
-      self.download_mp3_button = Button(self.center_frame, text="Download as MP3", command=self.downloadMP3)
-      self.download_mp3_button.place(rely=.95, relwidth=.25, relheight=.05)
+      self.download_mp4_button = Button(self.center_frame, text = "Download as MP4",
+                                        command = self.downloadMP4)
+      self.download_mp4_button.place(rely = .875, relwidth = .25, relheight = .05)
+      self.download_mp3_button = Button(self.center_frame, text = "Download as MP3",
+                                        command = self.downloadMP3)
+      self.download_mp3_button.place(rely = .95, relwidth = .25, relheight = .05)
       self.download_results = Text(self.center_frame)
-      self.download_results.place(relx=.3, rely=.875, relwidth=.7, relheight=.125)
+      self.download_results.place(relx = .3, rely = .875, relwidth = .7, relheight = .125)
 
-      self.back_button = Button(self, text="Back", command=self.goBack)
-      self.back_button.place(relwidth=.1, relheight=.05)
+      self.back_button = Button(self, text = "Back", command = self.goBack)
+      self.back_button.place(relwidth = .1, relheight = .05)
 
       self.protocol("WM_DELETE_WINDOW", self.closing)
 
@@ -124,8 +131,8 @@ class DownVid(Toplevel):
       if self.url == "":
          self.vid_info.insert(END, "Sorry. Please enter a link.")
       else:
-         with youtube_dl.YoutubeDL() as ydl:
-            data = ydl.extract_info(self.url, download=False)
+         with yt_dlp.YoutubeDL() as ydl:
+            data = ydl.extract_info(self.url, download = False)
             title = data["title"]
             uploader = data["uploader"]
             views = data["view_count"]
@@ -151,10 +158,11 @@ class DownVid(Toplevel):
       self.download_results.delete(1.0, END)
       if self.directory != "":
          ydl_options = {
-            "format": "bestvideo[ext=mp4][vcodec!*=av01]+bestaudio",  # Don't download AV1 format vids
+            "format": "bestvideo[ext=mp4][vcodec!*=av01]+bestaudio",
+            # Don't download AV1 format vids
             "outtmpl": self.directory + "/%(title)s.%(ext)s"
          }
-         with youtube_dl.YoutubeDL(ydl_options) as ydl:
+         with yt_dlp.YoutubeDL(ydl_options) as ydl:
             try:
                ydl.download([self.url])
                self.download_results.insert(END, "MP4 Download Complete!")
@@ -174,7 +182,7 @@ class DownVid(Toplevel):
                "preferredcodec": "mp3"
             }]
          }
-         with youtube_dl.YoutubeDL(ydl_options) as ydl:
+         with yt_dlp.YoutubeDL(ydl_options) as ydl:
             try:
                ydl.download([self.url])
                self.download_results.insert(END, "MP3 Download Complete!")
@@ -192,51 +200,54 @@ class DownVid(Toplevel):
 
 
 class DownPL(Toplevel):
-   def __init__(self, parent):  # If this is the second window, then you can add a root variable to tell it where the root is (if it is not the parent) and then destroy it that way
+   def __init__(self,
+                parent):  # If this is the second window, then you can add a root variable to tell it where the root is (if it is not the parent) and then destroy it that way
       self.parent = parent
-      print(self.parent)
       Toplevel.__init__(self, self.parent)
       self.url = ""
       self.directory = ""
       self.geometry("551x600")
       self.title("Playlist Downloader")
-      self.resizable(width=False, height=False)
+      # self.resizable(width=False, height=False)
 
-      self.picture = PhotoImage(file="Pictures/space dog.png")
-      self.outside_pic = Label(self, image=self.picture)
-      self.outside_pic.place(relwidth=1, relheight=1)
+      self.picture = PhotoImage(file = "Pictures/space dog.png")
+      self.outside_pic = Label(self, image = self.picture)
+      self.outside_pic.place(relwidth = 1, relheight = 1)
 
-      self.center_frame = Frame(self, bd=0)
-      self.center_frame.place(relx=.1, rely=.1, relwidth=.8, relheight=.8)
+      self.center_frame = Frame(self, bd = 0)
+      self.center_frame.place(relx = .1, rely = .1, relwidth = .8, relheight = .8)
 
-      self.inside_pic = Label(self.center_frame, image=self.picture)
-      self.inside_pic.place(relwidth=1, relheight=1)
+      self.inside_pic = Label(self.center_frame, image = self.picture)
+      self.inside_pic.place(relwidth = 1, relheight = 1)
 
-      self.enter_url = Entry(self.center_frame, bd=0)
-      self.enter_url.place(relwidth=.85, relheight=.05)
-      self.get_url = Button(self.center_frame, text="Get Url", command=self.getUrl)
-      self.get_url.place(relx=.87, relwidth=.13, relheight=.05)
+      self.enter_url = Entry(self.center_frame, bd = 0)
+      self.enter_url.place(relwidth = .85, relheight = .05)
+      self.get_url = Button(self.center_frame, text = "Get Url", command = self.getUrl)
+      self.get_url.place(relx = .87, relwidth = .13, relheight = .05)
 
-      self.vid_info = Text(self.center_frame, wrap="word")
-      self.vid_info.place(rely=.1, relwidth=.975, relheight=.65)
-      scrollbar = Scrollbar(self.center_frame, command=self.vid_info.yview)
-      scrollbar.place(relx=.975, rely=.1, relwidth=.025, relheight=.65)
+      self.vid_info = Text(self.center_frame, wrap = "word")
+      self.vid_info.place(rely = .1, relwidth = .975, relheight = .65)
+      scrollbar = Scrollbar(self.center_frame, command = self.vid_info.yview)
+      scrollbar.place(relx = .975, rely = .1, relwidth = .025, relheight = .65)
       self.vid_info["yscrollcommand"] = scrollbar.set
 
-      self.dir_button = Button(self.center_frame, text="Download to", command=self.getDirectory)
-      self.dir_button.place(rely=.8, relwidth=.25, relheight=.05)
+      self.dir_button = Button(self.center_frame, text = "Download to",
+                               command = self.getDirectory)
+      self.dir_button.place(rely = .8, relwidth = .25, relheight = .05)
       self.display_dir = Text(self.center_frame)
-      self.display_dir.place(relx=.3, rely=.8, relwidth=.7, relheight=.05)
+      self.display_dir.place(relx = .3, rely = .8, relwidth = .7, relheight = .05)
 
-      self.download_mp4_button = Button(self.center_frame, text="Download as MP4", command=self.downloadMP4)
-      self.download_mp4_button.place(rely=.875, relwidth=.25, relheight=.05)
-      self.download_mp3_button = Button(self.center_frame, text="Download as MP3", command=self.downloadMP3)
-      self.download_mp3_button.place(rely=.95, relwidth=.25, relheight=.05)
+      self.download_mp4_button = Button(self.center_frame, text = "Download as MP4",
+                                        command = self.downloadMP4)
+      self.download_mp4_button.place(rely = .875, relwidth = .25, relheight = .05)
+      self.download_mp3_button = Button(self.center_frame, text = "Download as MP3",
+                                        command = self.downloadMP3)
+      self.download_mp3_button.place(rely = .95, relwidth = .25, relheight = .05)
       self.download_results = Text(self.center_frame)
-      self.download_results.place(relx=.3, rely=.875, relwidth=.7, relheight=.125)
+      self.download_results.place(relx = .3, rely = .875, relwidth = .7, relheight = .125)
 
-      self.back_button = Button(self, text="Back", command=self.goBack)
-      self.back_button.place(relwidth=.1, relheight=.05)
+      self.back_button = Button(self, text = "Back", command = self.goBack)
+      self.back_button.place(relwidth = .1, relheight = .05)
 
       self.protocol("WM_DELETE_WINDOW", self.closing)
 
@@ -247,9 +258,8 @@ class DownPL(Toplevel):
          self.vid_info.insert(END, "Sorry. Please enter a link.")
       else:
          self.vid_info.insert(END, "VIDEOS IN PLAYLIST \n\n")
-         with youtube_dl.YoutubeDL() as ydl:
-            data = ydl.extract_info(self.url, download=False)
-            print(data)
+         with yt_dlp.YoutubeDL() as ydl:
+            data = ydl.extract_info(self.url, download = False)
             for video in data["entries"]:
                title = video["title"]
                uploader = video["uploader"]
@@ -275,11 +285,12 @@ class DownPL(Toplevel):
       self.download_results.delete(1.0, END)
       if self.directory != "":
          ydl_options = {
-            "format": "bestvideo[ext=mp4][vcodec!*=av01]+bestaudio",  # Don't download AV1 format vids
+            "format": "bestvideo[ext=mp4][vcodec!*=av01]+bestaudio",
+            # Don't download AV1 format vids
             "ignoreerrors": True,
             "outtmpl": self.directory + "/%(playlist_title)s/%(title)s.%(ext)s"
          }
-         with youtube_dl.YoutubeDL(ydl_options) as ydl:
+         with yt_dlp.YoutubeDL(ydl_options) as ydl:
             ydl.download([self.url])
             self.download_results.insert(END, "MP4 Download Complete!")
       else:
@@ -297,7 +308,7 @@ class DownPL(Toplevel):
                "preferredcodec": "mp3"
             }]
          }
-         with youtube_dl.YoutubeDL(ydl_options) as ydl:
+         with yt_dlp.YoutubeDL(ydl_options) as ydl:
             ydl.download([self.url])
             self.download_results.insert(END, "MP3 Download Complete!")
       else:
@@ -312,8 +323,9 @@ class DownPL(Toplevel):
 
 
 def main():
+   # ctypes.windll.shcore.SetProcessDpiAwareness(1)
    main = Tk()
-   opening_screen = Welcome(main)
+   Welcome(main)
    main.mainloop()
 
 
